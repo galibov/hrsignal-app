28acc08e4c36fd4b96cbc4bab753abf5e11e4c24
Anatoly <galibov@proton.me>
2023-02-04 18:29:32 +0200

    created form component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="text-center p-4">
+  <h1 class="text-3xl font-bold underline mb-4">Guess my info</h1>
+  <SendingForm/>
+</div>
+
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+import SendingForm from './components/SendingForm.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ frontend/src/components/SendingForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex justify-center">
+    <form @submit.prevent>
+    <div>
+      <input
+        type="text"
+        placeholder="Type your name"
+        class="input input-bordered w-full max-w-xs"
+      />
+    </div>
+    <div class="mt-2">
+      <label class="label cursor-pointer">
+        <span class="label-text mr-2">Agree to send my data to the 3rd party API</span>
+        <input type="checkbox" checked="checked" class="checkbox" />
+      </label>
+    </div>
+    <div class="mt-2">
+      <button type="submit" class="btn">Send</button>
+    </div>
+  
+  </form>
+  </div>
+
+</template>
+
+<script setup lang="ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
